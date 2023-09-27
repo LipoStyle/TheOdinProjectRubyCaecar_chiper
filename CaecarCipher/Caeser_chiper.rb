456b8462344d67513718834a8ef454130840ecf6
LipoStyle <39531813+LipoStyle@users.noreply.github.com>
2023-09-27 17:19:37 +0300

    Update Caeser_chiper.rb

--- CaecarCipher/Caeser_chiper.rb
+++ CaecarCipher/Caeser_chiper.rb
@@ -6,26 +6,20 @@ def checkMethod(letter, number)
   letters_upCased = ("A".."Z").to_a
   new_letter = ""
   #finding and replacing the letter to the new letter
-  if letter == " "
-    new_letter = " "
-  elsif letter.class == Numeric
-    new_letter = letter
-  else
-    if letters.include?(letter)
-      index = letters.index(letter) + number
-      if index > 25
-        index = 26 - index
-      end
-      new_letter = letters[index]
-    elsif letters_upCased.include?(letter)
-      index = letters_upCased.index(letter) + number
-      if index > 25
-        index = 26 - index
-      end
-      new_letter = letters_upCased[index]
-    else
-      new_letter = letter
+  if letters.include?(letter)
+    index = letters.index(letter) + number
+    if index > 25
+      index = 26 - index
     end
+    new_letter = letters[index]
+  elsif letters_upCased.include?(letter)
+    index = letters_upCased.index(letter) + number
+    if index > 25
+      index = 26 - index
+    end
+    new_letter = letters_upCased[index]
+  else
+    new_letter = letter
   end
   #returning the new letter
     return new_letter 
@@ -43,4 +37,4 @@ def caesar_cipher(string, number)
   return newString.join
 end
   # we call the method with our parameters
-  puts caesar_cipher("Hello There you !!12 3123  ", 3)
+  puts caesar_cipher("Hello There you ERT !!12 3123  ", 2)
